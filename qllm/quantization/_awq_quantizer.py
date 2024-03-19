38e582e7def3429f67c15a1da7df0f271dfcce35
wejoncy <wejoncy@163.com>
2024-03-19 20:21:28 +0800

    awq sym

--- qllm/quantization/_awq_quantizer.py
+++ qllm/quantization/_awq_quantizer.py
@@ -154,30 +154,25 @@ def pseudo_quantize_tensor(w, n_bit, q_config, inplace=False, get_scale_zp=False
         min_int = 0
         scales = (max_val - min_val).clamp(min=1e-5) / max_int
         zeros = (-torch.round(min_val / scales)).clamp_(min_int, max_int)
-    else:  # we actually never used this
-        assert min_val is None
+        w = (torch.clamp(torch.round(w / scales) + zeros, min_int, max_int) - zeros) * scales
+        zeros = zeros.view(org_w_shape[0], -1)
+    else:
         max_val = w.abs().amax(dim=1, keepdim=True)
         max_val = max_val.clamp(min=1e-5)
         max_int = 2 ** (n_bit - 1) - 1
         min_int = - 2 ** (n_bit - 1)
         scales = max_val / max_int
-        zeros = 0
+        zeros = None
+        w = torch.clamp(torch.round(w / scales), min_int, max_int) * scales
 
     assert torch.isnan(scales).sum() == 0
     assert torch.isnan(w).sum() == 0
 
-    if inplace:
-        ((w.div_(scales).round_().add_(zeros)).clamp_(
-            min_int, max_int).sub_(zeros)).mul_(scales)
-    else:
-        w = (torch.clamp(torch.round(w / scales) +
-                         zeros, min_int, max_int) - zeros) * scales
-    assert torch.isnan(w).sum() == 0
-
+    scales = scales.view(org_w_shape[0], -1)
     w = w.reshape(org_w_shape)
 
-    if get_scale_zp:
-        return w, scales.view(w.shape[0], -1), zeros.view(w.shape[0], -1)
+    if get_scale_zp:            
+        return w, scales.view(w.shape[0], -1), zeros
     else:
         return w
 
